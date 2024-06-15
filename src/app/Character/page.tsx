fefe9efb810002836bfd2b333c3b0b68c0e4957d
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-16 01:04:49 +0900

    warning解消

--- src/app/Character/page.tsx
+++ src/app/Character/page.tsx
@@ -11,7 +11,7 @@ export default async function dataIndex() {
         <h1 className="text-6xl mb-20 font-black">Character</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {data.map((item) => (
-            <div key={item.name} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div key={item.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             { item.image ? (
                 <div className="flex justify-center items-center h-48 overflow-hidden mt-1">
                 <img className="h-full object-contain rounded-t-lg" src={`${item.image}`} alt={item.name} />
@@ -36,7 +36,7 @@ export default async function dataIndex() {
                 <a href={`/${item.name}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
                 </div>
--- src/app/Spell/page.tsx
+++ src/app/Spell/page.tsx
@@ -23,8 +23,8 @@ const Spell = async () => {
             </thead>
             <tbody>
                 {
-                    data.map((item, index) => (
-                        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    data.map(( item ) => (
+                        <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.name}
                             </th>
